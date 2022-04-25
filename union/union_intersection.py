f243fe3ca8e5557bee9c9cf5b229bc01d3b3473b
seawweed <dongyun-kim@gimdong-yuns-MacBook-Air.local>
2022-04-25 17:23:57 +0900

    합집합, 교집합 코드 간소화

--- union/union_intersection.py
+++ union/union_intersection.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 def union(firstList, secondList):
-    interList = []
-    unionList = []
-
-    i, j = 0, 0
-    while i < len(firstList) and j < len(secondList):
-        if firstList[i] == secondList[j]:
-            interList.append(firstList[i])
-            unionList.append(firstList[i])
-            i += 1
-            j += 1
-        elif firstList[i] < secondList[j]:
-            unionList.append(firstList[i])
-            i += 1
-        else:
-            unionList.append(secondList[j])
-    while i < len(firstList):
-        unionList.append(firstList[i])
-        i += 1
-    while j < len(secondList):
-        unionList.append(firstList[j])
-        j += 1
+    set1 = set(firstList)
+    set2 = set(secondList)
+    interList = list(set1.intersection(set2))
+    unionList = list(set1.union(set2))
     print("union : ", unionList)
     print("intersection: ", interList)
